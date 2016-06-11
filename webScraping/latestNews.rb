d3da32f312e427c92775d289fc6fdc6fa38a081a
Nicholas H <syncopika@users.noreply.github.com>
2016-06-11 12:19:31 -0400

    Update latestNews.rb

--- webScraping/latestNews.rb
+++ webScraping/latestNews.rb
@@ -15,12 +15,10 @@ base_url4 = "http://www.animenewsnetwork.com/"
 #page_data = open(base_url, :allow_redirections => :all, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read
 #doc = Nokogiri::HTML(open(base_url, :allow_redirections => :all, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
 
-
 doc = Nokogiri::HTML(open(base_url))
 doc2 = Nokogiri::HTML(open(base_url2))
 doc3 = Nokogiri::HTML(open(base_url3))
 doc4 = Nokogiri::HTML(open(base_url4))
-
 
 latestNews = "lastestNews.html"
 file = File.open(latestNews, "w") #makes a new file called latestNews and writes to it
@@ -49,16 +47,22 @@ for i in 1..10
 end
 file.puts "</div>"
 
-#for trumpet herald: '//td[1]/table[1]/tr/td/a/text()'
-
 #for CNN
 file.puts "<div id='CNN'>"
 file.puts "<h2><b> latest news on CNN </b></h2>"
 #puts "list of recent CNN news topics"
 for i in 1..13
-	newPath = '//section[1]/div[2]/div/div[2]/ul/li[' + i.to_s + ']/article/div/div/h3/a/span'
+	#for some reason, for the 4th part of the path (div[2]) seems to vary between 2 or 3. 
+	#use an if/else to find the right one
+	testPath = '//section[1]/div[2]/div/div[2]/ul/li[1]/article/div/div/h3/a/span'
+	if doc2.xpath(testPath).text === ""
+		newPath = '//section[1]/div[2]/div/div[3]/ul/li[' + i.to_s + ']/article/div/div/h3/a/span'
+		urlPath = '//section[1]/div[2]/div/div[3]/ul/li[' + i.to_s + ']/article/div/div/h3/a' + '/@href'
+	else
+		newPath = '//section[1]/div[2]/div/div[2]/ul/li[' + i.to_s + ']/article/div/div/h3/a/span'
+		urlPath = '//section[1]/div[2]/div/div[2]/ul/li[' + i.to_s + ']/article/div/div/h3/a' + '/@href'
+	end
 	recentNewsCNN = doc2.xpath(newPath)
-    urlPath = '//section[1]/div[2]/div/div[2]/ul/li[' + i.to_s + ']/article/div/div/h3/a' + '/@href'
 	url = base_url2 + doc2.xpath(urlPath).text
 	
 	#found a weird case where the @href was the whole url, i.e. http://www.cnn.....
